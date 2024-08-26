fcf7426f0e67b96b0245077b48e0544d8a1f75a6
Matheus Paes Crescêncio <matheus.crescencio@portoseguro.com.br>
2024-08-26 14:27:02 -0300

    Removed unnecessary files.

--- Pods/Target Support Files/Pods-tip-calculator-tip-calculatorUITests/Pods-tip-calculator-tip-calculatorUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_tip_calculator_tip_calculatorUITests : NSObject
-@end
-@implementation PodsDummy_Pods_tip_calculator_tip_calculatorUITests
-@end
--- Pods/Target Support Files/Pods-tip-calculator/Pods-tip-calculator-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_tip_calculator : NSObject
-@end
-@implementation PodsDummy_Pods_tip_calculator
-@end
